f89037590bf262aa33dba824f46813eeede27c0e
Giddy <n.gideon.gitau@gmail.com>
2016-12-12 17:58:43 +0300

    (Feature) Adds security to methods

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,44 +1,67 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../../api/tasks.js';
- 
+
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
-
+    this.hideCompleted = false;
     this.helpers({
         tasks() {
-            // Show the newest tasks at the top
-            return Tasks.find({}, {
-              sort: {
-                createdAt: -1
-              }
-            });
-        }
+          const selector = {}
+
+          // If hideCompleted is checked, filter tasks
+          if (this.getReactively('hideCompleted')) {
+            selector.checked = {
+              $ne: true
+            };
+          }
+
+          // Show newest tasks at the top
+          return Tasks.find(selector, {
+            sort: {
+              createdAt: -1
+            }
+          });
+        },
+
+        currentUser() {
+          return Meteor.user()
+        },
+
+        incompleteCount() {
+          return Tasks.find({
+            checked: {
+              $ne: true
+            }
+          }).count()
+        },
     })
   }
 
+  
   addTask(newTask) {
       // Insert a task into the collection
-      Tasks.insert({
-          text: newTask,
-          createdAt: new Date()
-      });
+      Meteor.call('tasks.insert', newTask);
 
       // Clear form
       this.newTask = '';
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id)
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
 
 }
- 
-export default angular.module('todosList', [
-  angularMeteor
-])
+
+export default angular.module('todosList', [angularMeteor])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
     controller: ['$scope', TodosListCtrl]
